3967a4443a5dd12c021cf26d0b5830738edadadc
BamaJoe411 <bama@bamajoe411.com>
2021-03-01 03:00:51 -0500

    updated role assign and added rules accept

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -7,10 +7,17 @@ module.exports = async (client, reaction, user) => {
 			return;
 		}
     }
-
+    // add game role
     if (client.config.assignChannels.includes(reaction.message.channel.id)) {
         if (reaction._emoji.name == '☑️') {
             const role = reaction.message.guild.roles.cache.find(r => r.name === reaction.message.content);
+            const member = reaction.message.guild.members.cache.get(user.id);
+            member.roles.add(role).catch(console.error);
+        }
+    // add rules accepted role
+    } else if (client.config.rulesChannels.includes(reaction.message.channel.id)) {
+        if (reaction._emoji.name == '☑️') {
+            const role = reaction.message.guild.roles.cache.find(r => r.name === "Corner Dweller");
             const member = reaction.message.guild.members.cache.get(user.id);
             member.roles.add(role).catch(console.error);
         }
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,4 +1,4 @@
-module.exports = async (client, reaction) => {
+module.exports = async (client, reaction, user) => {
     if (reaction.partial) {
         try {
             await reaction.fetch();
@@ -7,10 +7,17 @@ module.exports = async (client, reaction) => {
             return;
         }
     }
-
+    // remove game role
     if (client.config.assignChannels.includes(reaction.message.channel.id)) {
         if (reaction._emoji.name == '☑️') {
             const role = reaction.message.guild.roles.cache.find(r => r.name === reaction.message.content);
+            const member = reaction.message.guild.members.cache.get(user.id);
+            member.roles.remove(role).catch(console.error);
+        }
+    // remove rules accepted role
+    } else if (client.config.rulesChannels.includes(reaction.message.channel.id)) {
+        if (reaction._emoji.name == '☑️') {
+            const role = reaction.message.guild.roles.cache.find(r => r.name === "Corner Dweller");
             const member = reaction.message.guild.members.cache.get(user.id);
             member.roles.remove(role).catch(console.error);
         }
